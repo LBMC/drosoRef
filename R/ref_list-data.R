5843503284d5ae3e004d5e176379391dbc902605
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2020-06-17 18:30:05 +0200

    Added the reflist doc

---
+++ R/ref_list-data.R
@@ -0,0 +1,13 @@
+#' List of reference in the wormRef data-package
+#' 
+#' This object lists the pre-built references available for use with RAPToR.
+#' 
+#' @docType data
+#' 
+#' @section Format:
+#' 
+#' A dataframe with the name of the reference, the organism, and a brief description.
+#' More information on the references is given on their documentation pages 
+#' (*e.g* \code{?Dme_embryo}).
+#' 
+"ref_list"

200357964d0ef511465ffd0e9a65b19951821ce7
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2022-08-26 18:02:58 +0200

    Made prepref function factory and applied to embryo ref.

--- R/prepref_Dme.R
+++ R/prepref_Dme.R
@@ -3,35 +3,50 @@
 #' Builds the interpolation of the reference datasets.
 #' **These functions are internally called by \code{\link[RAPToR]{prepare_refdata}} from RAPToR.**
 #' 
-#' @param n.inter the resolution of the interpolation, as in \code{seq(start, end, length.out = n.inter)}.
+#' @param n.inter,by.inter the resolution of the interpolation, as in \code{seq(start, end, length.out = n.inter)} or \code{seq(start, end, by = by.inter)}.
 #' 
 #' @return A list with \code{interpGE} the interpolated gene expression matrix and 
 #' \code{time.series} the time of the interpGE matrix columns.
 #' 
-#' @seealso \code{\link[RAPToR]{prepare_refdata}} \code{\link[RAPToR]{ge_im}}
+#' @seealso \code{\link[RAPToR]{prepare_refdata}} \code{\link[RAPToR]{ge_im}} \code{\link[RAPToR]{make_ref}}
 #' 
 #' @name Dme_prep
 NULL
 
+
 #' @rdname Dme_prep
 #' @export
-#' @importFrom RAPToR ge_im
+#' @importFrom RAPToR ge_im make_ref
 #' @importFrom stats predict
 #' 
-.prepref_Dme_embryo <- function(n.inter){
-  # utils::data("Dme_embryo", envir = environment())
-  m <- RAPToR::ge_im(
-    X = drosoRef::Dme_embryo$g,
-    p = drosoRef::Dme_embryo$p,
-    formula = drosoRef::Dme_embryo$geim_params$formula,
-    method = drosoRef::Dme_embryo$geim_params$method,
-    dim_red = drosoRef::Dme_embryo$geim_params$dim_red,
-    nc = drosoRef::Dme_embryo$geim_params$nc
-  )
-  ndat <- data.frame(age = seq(min(drosoRef::Dme_embryo$p$age),
-                               max(drosoRef::Dme_embryo$p$age),
-                               l = n.inter))
-  return(
-    list(interpGE = predict(m, ndat), time.series = ndat$age)
-  )
+.prepref_skel <- function(data, from=NULL, to=NULL){
+  # .prepref function factory
+  f <- function(n.inter=NULL, by.inter=NULL){
+    m <- RAPToR::ge_im(
+      X = data$g,
+      p = data$p,
+      formula = data$geim_params$formula,
+      method = data$geim_params$method,
+      dim_red = data$geim_params$dim_red,
+      nc = data$geim_params$nc
+    )
+    return(RAPToR::make_ref(m, 
+                            n.inter = n.inter,
+                            by.inter = by.inter,
+                            from = from, 
+                            to = to,
+                            t.unit = data$t.unit,
+                            cov.levels = data$cov.levels,
+                            metadata = data$metadata)
+    )
+  }
+  return(f)
 }
+
+
+
+
+#' @rdname Dme_prep
+#' @export
+#' 
+.prepref_Dme_embryo <- .prepref_skel(drosoRef::Dme_embryo)
